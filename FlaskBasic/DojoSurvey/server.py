15816be00dc87026bcab5d966f600c8c9f083c9b
unknown <hebertsa07@gmail.com>
2017-09-08 19:11:15 -0400

    adding dojo survey assignment

---
+++ FlaskBasic/DojoSurvey/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, request, session
+
+app = Flask(__name__)
+app.secret_key = '12345'
+
+@app.route('/')
+
+
+
+
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    
+    session['location'] = request.form['location']
+    session['favlang'] = request.form['FavLang']
+    session['name']= request.form['name']
+
+    
+    return redirect('/results')
+
+@app.route('/results')
+def results():
+    return render_template('results.html', firstName=session['name'], location=session['location'], favlang=session['favlang'])
+
+app.run(debug=True)
