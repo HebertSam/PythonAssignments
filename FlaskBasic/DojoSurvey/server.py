780b74e5f0a82297b134828623fd4964a729ff4c
unknown <hebertsa07@gmail.com>
2017-09-11 19:30:08 -0400

    adding new files

--- FlaskBasic/DojoSurvey/server.py
+++ FlaskBasic/DojoSurvey/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
 
 app = Flask(__name__)
 app.secret_key = '12345'
@@ -7,6 +7,11 @@ app.secret_key = '12345'
 
 
 def index():
+    session['location'] = None
+    session['favlang'] = None
+    session['name'] = None
+    session['comments'] = None
+
     return render_template('index.html')
 
 @app.route('/bob', methods=['POST'])
@@ -14,13 +19,22 @@ def process():
     
     session['location'] = request.form['location']
     session['favlang'] = request.form['FavLang']
-    session['name']= request.form['name']
+    if len(request.form['name']) < 1:
+        flash('Name can not be empty')
+        return redirect('/')
+    else:
+        session['name']= request.form['name']
 
+    if len(request.form['comment']) > 120:
+        flash('Sorry you comment is too long it can only be 120 characters')
+        return redirect('/')
+    else:
+        session['comments'] = request.form['comment']
     
     return redirect('/results')
 
 @app.route('/results')
 def results():
-    return render_template('results.html', firstName=session['name'], location=session['location'], favlang=session['favlang'])
+    return render_template('results.html', firstName=session['name'], location=session['location'], favlang=session['favlang'], comment=session['comments'])
 
 app.run(debug=True)
